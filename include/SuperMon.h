0a13422fa738201da64bb3c84d694465310e07c6
John Cunha <johnstrongedge@proton.me>
2024-04-25 15:27:57 +0100

    first commit

---
+++ include/SuperMon.h
@@ -0,0 +1,407 @@
+/*
+
+
+  OK, ya ready for some fun? HTML + CSS styling + javascript all in and undebuggable environment
+
+  one trick I've learned to how to debug HTML and CSS code.
+
+  get all your HTML code (from html to /html) and past it into this test site
+  muck with the HTML and CSS code until it's what you want
+  https://www.w3schools.com/html/tryit.asp?filename=tryhtml_intro
+
+  No clue how to debug javascrip other that write, compile, upload, refresh, guess, repeat
+
+  I'm using class designators to set styles and id's for data updating
+  for example:
+  the CSS class .tabledata defines with the cell will look like
+  <td><div class="tabledata" id = "switch"></div></td>
+
+  the XML code will update the data where id = "switch"
+  java script then uses getElementById
+  document.getElementById("switch").innerHTML="Switch is OFF";
+
+
+  .. now you can have the class define the look AND the class update the content, but you will then need
+  a class for every data field that must be updated, here's what that will look like
+  <td><div class="switch"></div></td>
+
+  the XML code will update the data where class = "switch"
+  java script then uses getElementsByClassName
+  document.getElementsByClassName("switch")[0].style.color=text_color;
+
+
+  the main general sections of a web page are the following and used here
+
+  <html>
+    <style>
+    // dump CSS style stuff in here
+    </style>
+    <body>
+      <header>
+      // put header code for cute banners here
+      </header>
+      <main>
+      // the buld of your web page contents
+      </main>
+      <footer>
+      // put cute footer (c) 2021 xyz inc type thing
+      </footer>
+    </body>
+    <script>
+    // you java code between these tags
+    </script>
+  </html>
+
+
+*/
+
+// note R"KEYWORD( html page code )KEYWORD"; 
+// again I hate strings, so char is it and this method let's us write naturally
+
+const char PAGE_MAIN[] PROGMEM = R"=====(
+
+<!DOCTYPE html>
+<html lang="en" class="js-focus-visible">
+
+<title>Web Page Motor Demo</title>
+
+  <style>
+    table {
+      position: relative;
+      width:100%;
+      border-spacing: 0px;
+    }
+    tr {
+      border: 1px solid white;
+      font-family: "Verdana", "Arial", sans-serif;
+      font-size: 20px;
+    }
+    th {
+      height: 20px;
+      padding: 3px 15px;
+      background-color: #343a40;
+      color: #FFFFFF !important;
+      }
+    td {
+      height: 20px;
+       padding: 3px 15px;
+    }
+    .tabledata {
+      font-size: 24px;
+      position: relative;
+      padding-left: 5px;
+      padding-top: 5px;
+      height:   25px;
+      border-radius: 5px;
+      color: #FFFFFF;
+      line-height: 20px;
+      transition: all 200ms ease-in-out;
+      background-color: #00AA00;
+    }
+    .pwmslider {
+      width: 50%;
+      height: 55px;
+      outline: none;
+      height: 25px;
+    }
+    .bodytext {
+      font-family: "Verdana", "Arial", sans-serif;
+      font-size: 24px;
+      text-align: left;
+      font-weight: light;
+      border-radius: 5px;
+      display:inline;
+    }
+    .navbar {
+      width: 100%;
+      height: 50px;
+      margin: 0;
+      padding: 10px 0px;
+      background-color: #FFF;
+      color: #000000;
+      border-bottom: 5px solid #293578;
+    }
+    .fixed-top {
+      position: fixed;
+      top: 0;
+      right: 0;
+      left: 0;
+      z-index: 1030;
+    }
+    .navtitle {
+      float: left;
+      height: 50px;
+      font-family: "Verdana", "Arial", sans-serif;
+      font-size: 50px;
+      font-weight: bold;
+      line-height: 50px;
+      padding-left: 20px;
+    }
+   .navheading {
+     position: fixed;
+     left: 60%;
+     height: 50px;
+     font-family: "Verdana", "Arial", sans-serif;
+     font-size: 20px;
+     font-weight: bold;
+     line-height: 20px;
+     padding-right: 20px;
+   }
+   .navdata {
+      justify-content: flex-end;
+      position: fixed;
+      left: 70%;
+      height: 50px;
+      font-family: "Verdana", "Arial", sans-serif;
+      font-size: 20px;
+      font-weight: bold;
+      line-height: 20px;
+      padding-right: 20px;
+   }
+    .category {
+      font-family: "Verdana", "Arial", sans-serif;
+      font-weight: bold;
+      font-size: 32px;
+      line-height: 50px;
+      padding: 20px 10px 0px 10px;
+      color: #000000;
+    }
+    .heading {
+      font-family: "Verdana", "Arial", sans-serif;
+      font-weight: normal;
+      font-size: 28px;
+      text-align: left;
+    }
+  
+    .btn {
+      background-color: #444444;
+      border: none;
+      color: white;
+      padding: 10px 20px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      margin: 4px 2px;
+      cursor: pointer;
+    }
+    .active {
+      background-color: #666666;
+      color: black;
+    }
+    .foot {
+      font-family: "Verdana", "Arial", sans-serif;
+      font-size: 20px;
+      position: relative;
+      height:   30px;
+      text-align: center;   
+      color: #AAAAAA;
+      line-height: 20px;
+    }
+    .container {
+      max-width: 1800px;
+      margin: 0 auto;
+    }
+    table tr:first-child th:first-child {
+      border-top-left-radius: 5px;
+    }
+    table tr:first-child th:last-child {
+      border-top-right-radius: 5px;
+    }
+    table tr:last-child td:first-child {
+      border-bottom-left-radius: 5px;
+    }
+    table tr:last-child td:last-child {
+      border-bottom-right-radius: 5px;
+    }
+    
+  </style>
+
+  <body style="background-color: #efefef" onload="process()">
+  
+    <header>
+      <div class="navbar fixed-top">
+          <div class="container">
+            <div class="navtitle">Motor Contorl Panel</div>
+            <div class="navdata" id = "date">mm/dd/yyyy</div>
+            <div class="navheading">DATE</div><br>
+            <div class="navdata" id = "time">00:00:00</div>
+            <div class="navheading">TIME</div>
+            
+          </div>
+      </div>
+    </header>
+  
+    <main class="container" style="margin-top:70px">
+      <div class="category">Encoder Reading</div>
+      <div style="border-radius: 10px !important;">
+      <table style="width:50%">
+      <colgroup>
+        <col span="1" style="background-color:rgb(230,230,230); width: 20%; color:#000000 ;">
+        <col span="1" style="background-color:rgb(200,200,200); width: 15%; color:#000000 ;">
+      </colgroup>
+      <col span="2"style="background-color:rgb(0,0,0); color:#FFFFFF">
+      <col span="2"style="background-color:rgb(0,0,0); color:#FFFFFF">
+      <tr>
+        <th colspan="1"><div class="heading">Sensor</div></th>
+        <th colspan="1"><div class="heading">Value</div></th>
+      </tr>
+      <tr>
+        <td><div class="bodytext">Magnetic Encoder</div></td>
+        <td><div class="tabledata" id = "e0"></div></td>
+      </tr>
+      </table>
+    </div>
+    <br>
+    <br>
+    <div class="category">Motor Controls</div>
+    <br>
+    <button type="button" class = "btn" id = "btnF">Forward</button>
+    </div>
+    <button type="button" class = "btn" id = "btnB">Backward</button>
+    </div>
+    <br>
+    <br>
+    <div class="bodytext">Number to send (PWM: <span id="pwmreceived"></span>)</div>
+    <br>
+    <input type="range" class="pwmslider" min="0" max="100" value = "0" width = "0%" oninput="UpdateSlider(this.value)"/>
+    <br>
+    <br>
+  </main>
+
+  <footer div class="foot" id = "temp" >ESP32 Encoder Monitor and Motor Control Demo</div></footer>
+  
+  </body>
+
+
+  <script type = "text/javascript">
+  
+    // global variable visible to all java functions
+    var xmlHttp=createXmlHttpObject();
+    var forward = document.getElementById("btnF");
+    var backward = document.getElementById("btnB");
+
+    // function to create XML object
+    function createXmlHttpObject(){
+      if(window.XMLHttpRequest){
+        xmlHttp=new XMLHttpRequest();
+      }
+      else{
+        xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      return xmlHttp;
+    }
+
+    forward.addEventListener("mousedown", function() { //button is pressed
+        forward.classList.add("active");
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("PUT", "Forward_1", false);
+        xhttp.send();
+    });
+
+    forward.addEventListener("mouseup", function() { //button is released
+        forward.classList.remove("active");
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("PUT", "Forward_0", false);
+        xhttp.send();
+    });
+
+    backward.addEventListener("mousedown", function() { //button is pressed
+        backward.classList.add("active");
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("PUT", "Backward_1", false);
+        xhttp.send();
+    });
+
+    backward.addEventListener("mouseup", function() { //button is released
+        backward.classList.remove("active");
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("PUT", "Backward_0", false);
+        xhttp.send();
+    });
+
+
+    function UpdateSlider(value) {
+      var xhttp = new XMLHttpRequest();
+      // this time i want immediate feedback to the fan speed
+      // yea yea yea i realize i'm computing fan speed but the point
+      // is how to give immediate feedback
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          // update the web based on reply from  ESP
+          document.getElementById("pwmreceived").innerHTML=this.responseText;
+        }
+      }
+      // this syntax is really weird the ? is a delimiter
+      // first arg UPDATE_SLIDER is use in the .on method
+      // server.on("/UPDATE_SLIDER", UpdateSlider);
+      // then the second arg VALUE is use in the processing function
+      // String t_state = server.arg("VALUE");
+      // then + the controls value property
+      xhttp.open("PUT", "PWM_update?VALUE="+value, true);
+      xhttp.send();
+    }
+
+    // function to handle the response from the ESP
+    function response(){
+      var message;
+      var barwidth;
+      var currentsensor;
+      var xmlResponse;
+      var xmldoc;
+      var dt = new Date();
+      var color = "#e8e8e8";
+     
+      // get the xml stream
+      xmlResponse=xmlHttp.responseXML;
+  
+      // get host date and time
+      document.getElementById("time").innerHTML = dt.toLocaleTimeString();
+      document.getElementById("date").innerHTML = dt.toLocaleDateString();
+  
+      // Encoder value
+      xmldoc = xmlResponse.getElementsByTagName("E0"); //E0 value
+      message = xmldoc[0].firstChild.nodeValue;
+      
+      if (message >= 0){
+      color = "#00aa00";
+      }
+      else {
+        color = "#aa0000";
+      }
+      
+      barwidth = message / 10;
+      document.getElementById("e0").innerHTML=message;
+      document.getElementById("e0").style.width=(barwidth+"%");
+      // if you want to use global color set above in <style> section
+      // other wise uncomment and let the value dictate the color
+      document.getElementById("b0").style.backgroundColor=color;
+      document.getElementById("b0").style.borderRadius="5px";
+
+     }
+  
+    // general processing code for the web page to ask for an XML steam
+    // this is actually why you need to keep sending data to the page to 
+    // force this call with stuff like this
+    // server.on("/xml", SendXML);
+    // otherwise the page will not request XML from the ESP, and updates will not happen
+    function process(){
+     
+     if(xmlHttp.readyState==0 || xmlHttp.readyState==4) {
+        xmlHttp.open("PUT","xml",true);
+        xmlHttp.onreadystatechange=response;
+        xmlHttp.send(null);
+      }       
+        // you may have to play with this value, big pages need more porcessing time, and hence
+        // a longer timeout
+        setTimeout("process()",100);
+    }
+  
+  
+  </script>
+
+</html>
+
+
+
+)=====";
---
+++ src/main.cpp
@@ -0,0 +1,288 @@
+#include <WiFi.h>       // standard library
+#include <WebServer.h>  // standard library
+#include "SuperMon.h"   // .h file that stores your html page code
+
+#include <Arduino.h>    // standard library
+#include <SPI.h>        // standard library
+
+// Coment out to use ESP32 as an access point
+#define USE_INTRANET
+
+// replace this with your homes intranet connect parameters
+// DEEC , uc2020252382@student.uc.pt , Joao2002
+// HUAWEI-89UJQF_HiLink , estudantes
+#define LOCAL_SSID "HUAWEI-89UJQF_HiLink"
+#define LOCAL_USER "uc2020252382@student.uc.pt"
+#define LOCAL_PASS "estudantes"
+
+// Settings for tne access point
+#define AP_SSID "TestWebSite"
+#define AP_PASS "023456789"
+
+// start your defines for pins for SPI, outputs, inputs, etc.
+#define VSPI_MISO 33
+#define VSPI_MOSI 25
+#define VSPI_SCLK 26
+#define VSPI_SS 27
+
+// initialize functions
+void printWifiStatus();     // Print WiFi settings and status
+void SendXML();             // Send the XML table to the Web
+void SendWebsite();         // Send HTML code to create a Web page
+void UpdatePWM();           // Callback when receives new PWM(slider) Value
+void ProcessForward_0();    // Callback when receives Forward = 0
+void ProcessForward_1();    // Callback when receives Forward = 1
+void ProcessBackward_0();   // Callback when receives Backward = 0
+void ProcessBackward_1();   // Callback when receives Backward = 1
+
+// variables to store measure data and sensor states
+uint32_t sensor_update_time = 0;
+int32_t PWM_slider = 0;
+
+// the XML array size needs to be bigger that your maximum expected size. 2048 is way too big for this example
+char XML[2048];
+// just some buffer holder for char operations
+char buf[32];
+
+// variable for the IP reported when you connect to your homes intranet (during debug mode)
+IPAddress Actual_IP;
+
+// definitions of your desired intranet created by the ESP32
+IPAddress PageIP(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress ip;
+
+// gotta create a server
+WebServer server(80);
+
+//--------------------------------------Start Dylan-------------------------------------
+
+#define bufferSize 10   // SPI Buffer size
+
+// Create a variable(union) to receive int32_t of data over SPI
+typedef union _packet_buffer_t
+{
+  uint8_t buff[4];
+  int32_t value;
+} packet_buffer_t;
+
+packet_buffer_t motor_encoder;     // Packet to store Motor Counter
+packet_buffer_t PWM_toSend;        // Packet to send Motor Speed
+
+uint8_t RxBuffer[bufferSize]; // Recive Buffer
+uint8_t TxBuffer[bufferSize]; // Transmit Buffer
+
+void setup() {
+
+  pinMode(VSPI_MISO, INPUT);    // Master IN Slave OUT -- INPUT
+  pinMode(VSPI_MOSI, OUTPUT);   // Master OUT Slave IN -- OUTPUT  
+  pinMode(VSPI_SCLK, OUTPUT);   // Master Clock OUT -- OUTPUT  
+  pinMode(VSPI_SS, OUTPUT);     // Master Chip Select -- OUTPUT  
+
+  SPI.begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI); // Begin SPI
+  SPI.setBitOrder(MSBFIRST);                  // Set Bit Order -- Most Significant Bit First
+  SPI.setDataMode(SPI_MODE0);                 // Set Data Transfer Mode -- Mode 0
+  SPI.setFrequency(1000000);                  // Set Transfer Speed -- 1 000 000
+
+  Serial.begin(115200);                 // Serial Connection Begin -- 9600
+  Serial.setDebugOutput(true);        // Set Debug Output
+
+  PWM_toSend.value = PWM_slider;
+
+    // if your web page or XML are large, you may not get a call back from the web page
+    // and the ESP will think something has locked up and reboot the ESP
+    // not sure I like this feature, actually I kinda hate it
+    // disable watch dog timer 0
+    //disableCore0WDT();
+
+    // maybe disable watch dog timer 1 if needed
+    //  disableCore1WDT();
+
+  Serial.println("starting server");  // just an update to progress
+
+  // if you have this #define USE_INTRANET,  you will connect to your home intranet, again makes debugging easier
+  #ifdef USE_INTRANET
+    WiFi.begin(LOCAL_SSID, LOCAL_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.print("IP address: "); Serial.println(WiFi.localIP());
+    Actual_IP = WiFi.localIP();
+  #endif
+
+  // if you don't have #define USE_INTRANET, here's where you will creat and access point
+  #ifndef USE_INTRANET
+    WiFi.softAP(AP_SSID, AP_PASS);
+    delay(100);
+    WiFi.softAPConfig(PageIP, gateway, subnet);
+    delay(100);
+    Actual_IP = WiFi.softAPIP();
+    Serial.print("IP address: "); Serial.println(Actual_IP);
+  #endif
+
+  printWifiStatus();  
+
+  //       These calls will handle data coming back from your web page
+
+  // this one is a page request, upon ESP getting / string the web page will be sent
+  server.on("/", SendWebsite);
+
+  // upon esp getting /XML string, ESP will build and send the XML, this is how we refresh
+  // just parts of the web page
+  server.on("/xml", SendXML);
+
+  // upon ESP getting /UPDATE_SLIDER string, ESP will execute the UpdateSlider function
+  // same notion for the following .on calls
+  // add as many as you need to process incoming strings from your web page
+  // as you can imagine you will need to code some javascript in your web page to send such strings
+  // this process will be documented in the SuperMon.h web page code
+  server.on("/PWM_update", UpdatePWM);
+  server.on("/Forward_0", ProcessForward_0);
+  server.on("/Forward_1", ProcessForward_1);
+  server.on("/Backward_0", ProcessBackward_0);
+  server.on("/Backward_1", ProcessBackward_1);
+
+  // finally begin the server
+  server.begin();
+}
+
+void loop() {
+
+  // you main loop that measures, processes, runs code, etc.
+  // note that handling the "on" strings from the web page are NOT in the loop
+  // that processing is in individual functions all managed by the wifi lib
+
+  // in my example here every 50 ms, i measure some analog sensor data (my finger dragging over the pins
+  // and process accordingly
+  // analog input can be from temperature sensors, light sensors, digital pin sensors, etc.
+  if ((millis() - sensor_update_time) >= 50) {
+    sensor_update_time = millis();
+    TxBuffer[5] = PWM_toSend.buff[0];
+    TxBuffer[6] = PWM_toSend.buff[1];
+    TxBuffer[7] = PWM_toSend.buff[2];
+    TxBuffer[8] = PWM_toSend.buff[3];
+    digitalWrite(VSPI_SS,LOW);
+    SPI.transferBytes(TxBuffer, RxBuffer, bufferSize);
+    digitalWrite(VSPI_SS, HIGH);
+    motor_encoder.buff[0] = RxBuffer[5];
+    motor_encoder.buff[1] = RxBuffer[6];
+    motor_encoder.buff[2] = RxBuffer[7];
+    motor_encoder.buff[3] = RxBuffer[8];
+    Serial.printf("Rx: %d \t | \t Tx: %d\n\r", motor_encoder.value, PWM_toSend.value);
+  }
+
+  // no matter what you must call this handleClient repeatidly--otherwise the web page
+  // will not get instructions to do something
+  server.handleClient();
+}
+
+// function managed by an .on method to handle slider actions on the web page
+void UpdatePWM() {
+
+  String t_state = server.arg("VALUE");
+
+  // conver the string sent from the web page to an int
+  PWM_slider = t_state.toInt();
+  Serial.print("Update PWM"); Serial.println(PWM_slider);
+
+  // YOU MUST SEND SOMETHING BACK TO THE WEB PAGE--BASICALLY TO KEEP IT LIVE
+
+  // option 1: send no information back, but at least keep the page live
+  // just send nothing back
+  // server.send(200, "text/plain", ""); //Send web page
+
+  // option 2: send something back immediately, maybe a pass/fail indication, maybe a measured value
+  // here is how you send data back immediately and NOT through the general XML page update code
+  // my simple example guesses at fan speed--ideally measure it and send back real data
+  // i avoid strings at all caost, hence all the code to start with "" in the buffer and build a
+  // simple piece of data
+  strcpy(buf, "");
+  sprintf(buf, "%d", PWM_slider);
+  sprintf(buf, buf);
+
+  // now send it back
+  server.send(200, "text/plain", buf); //Send web page
+}
+
+// processing Forward
+void ProcessForward_0() {
+  PWM_toSend.value = 0 ;
+  sprintf(buf, "%d", PWM_toSend.value);
+  server.send(200, "text/plain", buf); //Send web page
+}
+void ProcessForward_1() {
+  PWM_toSend.value = PWM_slider ;
+  sprintf(buf, "%d", PWM_toSend.value);
+  server.send(200, "text/plain", buf); //Send web page
+}
+// processing Backward
+void ProcessBackward_0() {
+  PWM_toSend.value = 0 ;
+  sprintf(buf, "%d", PWM_toSend.value);
+  server.send(200, "text/plain", buf); //Send web page
+}
+void ProcessBackward_1() {
+  PWM_toSend.value = - PWM_slider ;
+  sprintf(buf, "%d", PWM_toSend.value);
+  server.send(200, "text/plain", buf); //Send web page
+}
+
+// code to send the main web page
+// PAGE_MAIN is a large char defined in SuperMon.h
+void SendWebsite() {
+
+  Serial.println("sending web page");
+  // you may have to play with this value, big pages need more porcessing time, and hence
+  // a longer timeout that 200 ms
+  server.send(200, "text/html", PAGE_MAIN);
+
+}
+
+// code to send the main web page
+// I avoid string data types at all cost hence all the char mainipulation code
+void SendXML() {
+
+  // Serial.println("sending xml");
+
+  strcpy(XML, "<?xml version = '1.0'?>\n<Data>\n");
+
+  // send bitsA0
+  sprintf(buf, "<E0>%d</E0>\n",motor_encoder.value);
+  strcat(XML, buf);
+  
+  strcat(XML, "</Data>\n");
+  // wanna see what the XML code looks like?
+  // actually print it to the serial monitor and use some text editor to get the size
+  // then pad and adjust char XML[2048]; above
+  //Serial.println(XML);
+
+  // you may have to play with this value, big pages need more porcessing time, and hence
+  // a longer timeout that 200 ms
+  server.send(200, "text/xml", XML);
+}
+
+// I think I got this code from the wifi example
+void printWifiStatus() {
+
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  // print where to go in a browser:
+  Serial.print("Open http://");
+  Serial.println(ip);
+}
+
+// end of code
